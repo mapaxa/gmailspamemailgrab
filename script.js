4aca3a1c0d6df10fb280d079bf7555f16e667202
mapaxa <alexeybespalov86@gmail.com>
2019-03-06 10:11:32 +0200

    Update script.js
    added part with checking if tag exists

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 var arrayNotValid = []; // массив для невалидных
 
 function start(counter){
-  if(counter < 100){
+  if(counter < 256){
+
     setTimeout(function(){
       counter++; // счетчик
-      //var prevButton = document.querySelector('div>div>div>div>div>div>div>div>div>span+div[role="button"]:nth-child(2)');
-      var prevButton = document.querySelector('div>div>div>div>div>div>div>div:nth-of-type(2)>div>div>div[role="button"]:nth-of-type(1)'); // кнопка для автоматического нажатия
+
+    var prevButton = document.querySelector('div>div>div>div>div>div>div>div:nth-of-type(2)>div>div>div[role="button"]:nth-of-type(1)'); // кнопка для автоматического нажатия
+
+    var elem = document.querySelector('table>tbody>tr>td>table>tbody>tr>td>a');
+
+    if(elem == null){ // если такого элемента нет
+      console.log('her');
+    }
+    else if(elem.tagName === 'A') { // если это действительно a
+      if(elem.hasAttribute("href")){
+        console.log('href');
+          var email = elem.getAttribute('href'); // вытаскиваем из ссылки аттрибут href
+
+          var symbolNumber = (email.lastIndexOf('?email=') + 7); // ищу местоположение подстроки
+          var res = email.substr(symbolNumber);
+          arrayNotValid.push(res);  // пушу в массив
+          
+        }
+      }
       
-      //var email = document.querySelector('div>div> br + a:nth-of-type(5)').innerHTML; 
-      var email = document.querySelector('table>tbody>tr>td>table>tbody>tr>td>a').getAttribute('href'); // вытаскиваем из ссыдки аттрибут href
-      
-			var symbolNumber = (email.lastIndexOf('?email=') + 7); // ищу местоположение подстроки
-			var res = email.substr(symbolNumber);
-      arrayNotValid.push(res);  // пушу в массив
-      prevButton.click(); // триггер
+prevButton.click(); // триггер
       start(counter); // рекурсивно
     }, 4000);
   }
